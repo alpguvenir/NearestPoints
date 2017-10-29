658924d3adebade4bb55a67b708b389f548a1a9c
Alp Guvenir <alpguvenir@gmail.com>
2017-10-29 18:34:19 +0200

    New Project Initialized

---
+++ README.md
@@ -0,0 +1,98 @@
+# NearestPoints
+
+The purpose of the project is to read a text file where each line contains the coordinates of a multidimensional point. Furthermore, looks for the closest pair of points in the file. If the program finds the closest pair of points, it outputs the line numbers and the coordinates of the two closest points in to a file.
+
+The NearestCoordinates project is written in Java language, following the coding conventions stated in Oracle website.
+
+# Asymptotical Running Time
+
+The algorithm works in the following way:
+
+```java
+// Outer loop iterating on rows
+for rowIndex = 0 to coordinateMatrix.length - 1                 // (N-1) times
+  // Assign row1
+  row1 <- coordinateMatrix[rowIndex]
+
+  // Inner loop for iterating on consecutive rows
+  for innerRowIndex = rowIndex + 1 to coordinateMatrix.length   // (((N-1)+1)/2) times
+    // distance reinitialized to 0
+    squaredDistance <- 0
+
+    // Assign row2
+    row2 <- coordinateMatrix[innerRowIndex]
+
+    for columnIndex = 0 to row1.length                           // (M) times
+      squaredDistance += (row1[columnIndex] - row2[columnIndex])^2
+      
+    // Since we do not need the absolute distance values, we no dot need to take the square root**
+    // sqrtDistance = distance^(1/2)
+
+    // Euclidean distance comparison
+    if  squaredDistance < minimumDistance
+      minRowIndex1 <- rowIndex + 1
+      minRowIndex2 <- innerRowIndex + 1
+
+      // Assign the new minimum distance
+      minimumDistance <- squaredDistance
+```
+
+Here, 'N' stans for the number of points in our file of coordinates and 'M' stands for the number of dimensions.
+The outer loop executes (N) times, since in the worst case we are going to iterate to the coordinate 1 before the last, which is in the order of O(N).
+The inner loop executes ((N-1)+1)/2) times, since 'row2' is initialized from the next coordinate of 'row1', which is in the order of O(N).
+
+As it was stated before, the points are in a multidimensional coordinate system. Therefore, we take square root of the sum of squares of the differences of corresponding coordinate values. Since there are 'M' number of dimension, this operation is performed in the order of O(M).
+
+Thus, the algorithm works in the order of O(N<sup>2</sup>M).
+
+# Limitations
+
+The program takes the path of a '.tsv' file as an input from the user. Therefore at each execution, give path for only **one** file.
+The program finds the nearest pair of coordinates if:
+ - The text file contains one point per line
+ - The coordinate values are separated by a tabulator character
+ - The file consists of only integers or floating point numbers
+ - Floating point number use period as the decimal separator
+ - The file contains no missing feature value(s)
+
+The program assumes that values given for each dimension are at the same scale. Therefore, no feature normalization is done.
+
+The program asks for the name of the output '.txt' file, without giving the '.txt' extension, and the path for the file to be saved into. The program assumes the user enters a valid path.
+
+The user should be aware that if such a '.txt' file exists with the given name at the given path, the program will override the file.
+
+# Usage
+For using the NearestCoordinates, simply clone the project using 'git clone' command. Compile the project.
+
+A sample run is given below...
+Lines that start with '+' symbol indicate the user entry, just for clarification purposes, **do not** enter your input starting with a '+' sign':
+
+```diff
+Please provide the path of the tab seperated input file:
++/Users/AlpGuvenir/Desktop/n2c/sample_in_out/sample_input_2_8.tsv
+The file provided is valid
+Points with minimum distance are at:
+Row:3 and Row:6
+Time elasped: 138 nanoseconds
+Please provide a name for the output file,
+Do not put any extensions, it is automatically saved as a '.txt' file:
++sample_output_2_8
+Please provide the path of the output '.txt' file,
+the location you would like the output file to be saved:
++/Users/AlpGuvenir/Desktop/sample_output
+
+Process finished with exit code 0
+```
+
+# Timing experiment
+
+For each of the sample inputs provided, a timing experiment is made.
+The program is executed for each of the sample files 3 times:
+
+|                       |    Trial 1             |   Trial 2             | Trial 3               |
+|-----------------------|-----------------------:|----------------------:|----------------------:|
+|sample_input_2_8       |    138     nanoseconds |   176    nanoseconds  |   200     nanoseconds |
+|sample_input_3_1000    |    38650   nanoseconds |   26385  nanoseconds  |   63300   nanoseconds |
+|sample_input_4_4       |    148     nanoseconds |   147    nanoseconds  |   184     nanoseconds |
+|sample_input_10_100    |    5218    nanoseconds |   7716   nanoseconds  |   6414    nanoseconds |
+|sample_input_100_100   |    26815   nanoseconds |   17581  nanoseconds  |   22630   nanoseconds |
---
+++ src/NearestPoints.java
@@ -0,0 +1,231 @@
+/*
+ * NearestCoordinates.java
+ * 29 October 2017
+ * Version 1.0.0
+ *  Alp Güvenir
+*/
+
+// /Users/AlpGuvenir/Desktop/n2c/sample_in_out/sample_input_2_8.tsv
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Scanner;
+
+public class NearestPoints {
+
+    public static void main(String[] args) throws IOException {
+        // Scanner definition
+        Scanner keyboard = new Scanner(System.in);
+
+        // Path of the TSV file
+        String path = getPath(keyboard);
+
+        // Copy the values in TSV file to a 2D Double array
+        Double[][] coordinateMatrix = tsvToMatrix(path);
+
+        // Print the matrix filled from the file
+        // printMatrix(coordinateMatrix);
+
+        // Array for holding the pair of coordinates
+        int[] pair;
+
+        // Declarations for timing
+        long startTime;
+        long endTime;
+
+        // Find the nearest coordinates
+        if(coordinateMatrix.length > 1) {
+
+            // Timing begin
+            startTime = System.nanoTime();
+            pair = nearestCoordinates(coordinateMatrix);
+            // Timing end
+            endTime = System.nanoTime();
+            System.out.println("Time elasped: " + (endTime - startTime)/1000 + " nanoseconds");
+
+            // Write the output into a txt file
+            writeOutput(pair[0], pair[1], keyboard, coordinateMatrix[pair[0]], coordinateMatrix[pair[1]]);
+        }
+        else
+            System.out.println("There is only one point in the file, nothing to compare");
+    } // main()
+
+    public static String getPath(Scanner keyboard) {
+        System.out.println("Please provide the path of the tab seperated input file: ");
+        String inputPath = keyboard.next();
+        return inputPath;
+    } // getPath()
+
+    public static String setPath(Scanner keyboard) {
+        System.out.println("Please provide the path of the output \'.txt\' file, \nthe location you would like the output file to be saved: ");
+        String outputPath = keyboard.next();
+        return outputPath;
+    } // setPath()
+
+    public static String setFileName(Scanner keyboard) {
+        System.out.println("Please provide a name for the output file,\nDo not put any extensions, it is automatically saved as a \'.txt\' file: ");
+        String outputPath = keyboard.next();
+        outputPath += ".txt";
+        return outputPath;
+    } // setFileName()
+
+    public static Double[][] tsvToMatrix(String path) throws IOException {
+        // Store the TSV values in a double (2D) array
+        String[] row;
+        Double[][] resultArray = null;
+
+        int rowIndex = 0;
+        int colIndex = 0;
+
+        try {
+            // Get the TSV row as a List of String
+            List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+
+            // Find the number of columns from the length of the first row
+            row = lines.get(0).split("\t");
+            resultArray = new Double[lines.size()][row.length];
+
+            for (rowIndex = 0; rowIndex < lines.size(); rowIndex++) {
+                //Fill each row of the matrix by splitting the tab-separated row
+                row = lines.get(rowIndex).split("\t");
+
+                for (colIndex = 0; colIndex < row.length; colIndex++) {
+                    resultArray[rowIndex][colIndex] = Double.parseDouble(row[colIndex]);
+                } // Iterating inside a row
+
+            } // Iterating for each row
+
+        } catch (NoSuchFileException e) {
+            // If such a file does not exist
+            System.out.println("The path provided for the file is not valid\n" + e);
+            System.exit(1);
+
+        } catch (IndexOutOfBoundsException e) {
+            // If the file is empty
+            System.out.println("The file provided is empty\n" + e);
+            System.exit(1);
+
+        } catch (NumberFormatException e) {
+            System.out.println("The input in the file provided is in the wrong format\n" + e);
+            System.out.println("Error on row:" + rowIndex + " and column: " + colIndex);
+            System.exit(1);
+
+        } finally {
+            System.out.println("The file provided is valid");
+        }
+
+        return resultArray;
+    } // tsvToMatrix()
+
+    public static void printMatrix(Double[][] coordinateMatrix) {
+
+        for(int rowIndex = 0; rowIndex < coordinateMatrix.length; rowIndex++) {
+            for(int colIndex = 0; colIndex < coordinateMatrix[0].length; colIndex++)
+                System.out.print(coordinateMatrix[rowIndex][colIndex] + " ");
+            System.out.println();
+        }
+    } // printMatrix()
+
+    public static int[] nearestCoordinates(Double[][] coordinateMatrix) {
+        // Size is 2 since we are looking for a pair
+        int[] nearestCoordinates = new int[2];
+        double minimumDistance = Double.POSITIVE_INFINITY;
+        double squaredDistance = 0;
+        double squareroottDistance = 0;
+
+        // Two Double arrays for calculations
+        Double[] row1 = null;
+        Double[] row2 = null;
+
+        // Row number of two nearest coordinates, initally -1
+        nearestCoordinates[0] = -1;
+        nearestCoordinates[1] = -1;
+
+        // Outer loop iterating on rows
+        for(int rowIndex = 0; rowIndex < coordinateMatrix.length - 1; rowIndex++) { // In the order of O(N-1)
+
+            // Assign row1
+            row1 = coordinateMatrix[rowIndex];
+
+            // Inner loop for iterating on consecutive rows
+            for(int innerRowIndex = rowIndex + 1; innerRowIndex < coordinateMatrix.length; innerRowIndex++) { // In the order of O(((N-1)+1)/2)
+
+                // distance reinitialized to 0
+                squaredDistance = 0;
+
+                // Assign row2
+                row2 = coordinateMatrix[innerRowIndex];
+                for(int columnIndex = 0; columnIndex < row1.length; columnIndex++) { // In the order of O(M)
+                    squaredDistance += Math.pow((row1[columnIndex] - row2[columnIndex]), 2);
+                }
+
+                // Taking the square root
+                // Since we do not need the absolute distance values, we no dot need to take the square root**
+                // squarerootDistance = Math.sqrt(distance);
+
+                // Euclidean distance comparison
+                if(squaredDistance < minimumDistance) {
+                    nearestCoordinates[0] = rowIndex;
+                    nearestCoordinates[1] = innerRowIndex;
+
+                    // Assign the new minimum distance
+                    minimumDistance = squaredDistance;
+                }
+            }
+        }
+
+        System.out.println("Points with minimum distance are at:");
+        System.out.println("Row:" + (nearestCoordinates[0] + 1) + " and Row:" + (nearestCoordinates[1] + 1));
+
+        return nearestCoordinates;
+    } // nearestCoordinates()
+
+    public static void writeOutput(int minRowIndex1, int minRowIndex2, Scanner keyboard, Double[] row1, Double[] row2) throws IOException {
+
+        DecimalFormat df = new DecimalFormat("0.#");
+
+        // Name of the output '.txt' file
+        String outputFileName = setFileName(keyboard);
+
+        // Path for saving the file
+        String outputPath = setPath(keyboard);
+
+        // The output text
+        int index = 0;
+
+        // First row
+        String outputText = (minRowIndex1 + 1) + ":"; // minRowIndex1 + 1 since index starts from 0
+        for(index = 0; index < row1.length - 1; index++) {
+            outputText += df.format(row1[index]) + "\t";
+        }
+        outputText += df.format(row1[index]) + "\n";
+
+        // Second row
+        outputText += (minRowIndex2 + 1) + ":"; // minRowIndex2 + 1 since index starts from 0
+        for(index = 0; index < row2.length - 1; index++) {
+            outputText += df.format(row2[index]) + "\t";
+        }
+        outputText += df.format(row2[index]) + "\n";
+
+        // File creation
+        File directory = new File (outputPath);
+        File output = new File (directory, outputFileName);
+
+        // Using buffered writer to write the file
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(output));
+            out.write(outputText);
+            out.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("The path provided is not valid\n" + e);
+            System.exit(1);
+        }
+
+    } // writeOutput()
+
+}
